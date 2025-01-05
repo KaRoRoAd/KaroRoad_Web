0554712103f1c4fc85ba5d8244363d7eac8739ec
s27444-pj <s27444@pjwstk.edu.pl>
2025-01-05 14:37:23 +0100

    fix problem with docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-if [ ! -d "vendor" ]; then
-    composer install
-fi
-
 php bin/console cache:clear
 
 php bin/console doctrine:migrations:migrate --no-interaction
-
-php bin/console messenger:consume async -vv
-
-php bin/console lexik:jwt:generate-keypair
 
 exec apache2-foreground
